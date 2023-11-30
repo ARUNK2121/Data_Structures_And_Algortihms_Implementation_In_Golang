7a58e2da5e4a4bd8bf51e2ffe8d0e0128b30f294
Arun K <arthurbishop120@gmail.com>
2023-11-30 13:45:37 +0530

    added stack implementation using linked lists

---
+++ Stack/stack_using_linkedlists.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Stack struct {
+	Head     *Node
+	Capacity int
+	Length   int
+}
+
+func NewStackUsingList(size int) *Stack {
+	return &Stack{
+		Head:     nil,
+		Capacity: size,
+	}
+}
+
+type Node struct {
+	Data int
+	Next *Node
+}
+
+func (s *Stack) Push(data int) {
+	if s.Capacity == s.Length {
+		fmt.Println("stack overflow")
+		return
+	}
+	newNode := &Node{Data: data}
+	if s.Head == nil {
+		s.Head = newNode
+		s.Length++
+		return
+	}
+	newNode.Next = s.Head
+	s.Head = newNode
+	s.Length++
+}
+
+func (s *Stack) Pop(data int) (int, error) {
+	if s.Length == 0 {
+		return 0, errors.New("stack underflow")
+	}
+	popped := s.Head.Data
+	s.Head = s.Head.Next
+	return popped, nil
+}
+
+func (s *Stack) Peek(data int) {
+	fmt.Println("the value popped to be next:", s.Head.Data)
+}
